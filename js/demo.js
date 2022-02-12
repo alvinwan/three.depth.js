1d3bc31c5037bc428ec8a3b82b8e5d337b8a63c4
Alvin Wan <alvinwan1@gmail.com>
2022-02-12 03:12:02 -0800

    demo to toggle between rgb and depth

---
+++ js/demo.js
@@ -0,0 +1,42 @@
+function setupTorusKnotScene(count=50, scale=5) {
+    scene = new THREE.Scene();
+
+    const geometry = new THREE.TorusKnotGeometry( 1, 0.3, 128, 64 );
+    const material = new THREE.MeshPhongMaterial( { color: 'blue' } );
+
+    for ( let i = 0; i < count; i ++ ) {
+
+        const r = Math.random() * 2.0 * Math.PI;
+        const z = ( Math.random() * 2.0 ) - 1.0;
+        const zScale = Math.sqrt( 1.0 - z * z ) * scale;
+
+        const mesh = new THREE.Mesh( geometry, material );
+        mesh.position.set(
+            Math.cos( r ) * zScale,
+            Math.sin( r ) * zScale,
+            z * scale
+        );
+        mesh.rotation.set( Math.random(), Math.random(), Math.random() );
+        scene.add( mesh );
+    }
+
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        scene.add(light);
+    }
+}
+
+
+function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+}
