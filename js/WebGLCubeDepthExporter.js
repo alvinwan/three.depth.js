a6b0fca62db94ecf16aa4c0534bea94e05549edd
Alvin Wan <alvinwan1@gmail.com>
2022-02-12 02:23:41 -0800

    added exporter for cube depth

---
+++ js/WebGLCubeDepthExporter.js
@@ -0,0 +1,27 @@
+class WebGLCubeDepthExporter extends WebGLDepthExporter {
+    render(scene, cubeCamera) {
+        // Get original render target
+        var renderTarget = this.renderer.getRenderTarget();
+        var activeCubeFace = this.renderer.getActiveCubeFace();
+
+        // Render the depth texture to all faces of the cube, one at a time
+        for (let curCubeFace = 0; curCubeFace < 6; curCubeFace++) {
+            let invisibleCamera = cubeCamera.children[curCubeFace]; // camera in invisible space
+
+            // draw render target scene to render target
+            this.renderer.setRenderTarget(this.invisibleRenderTarget);
+            this.renderer.render(scene, invisibleCamera);
+            this.renderer.setRenderTarget(null);
+            
+            // render the depth texture to the provided target
+            this.renderer.setRenderTarget(renderTarget, curCubeFace);
+            this.renderer.render(this.depthScene, this.depthCamera);
+            this.renderer.setRenderTarget(null);
+        }
+
+        // Restore original render target
+        this.renderer.setRenderTarget(renderTarget, activeCubeFace);
+    }
+}
+
+THREE.WebGLCubeDepthExporter = WebGLCubeDepthExporter;
